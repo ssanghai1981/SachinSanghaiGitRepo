358d91485a0096908a1ebf8b75d25d6d011899b1
Sachinnagare <sachin.nagare3@gmail.com>
2021-10-07 22:29:34 +0530

    SN - adding new file

---
+++ GitTestProject/src/testPackage/SNjavaFile.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class SNjavaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Bavdhankar citizen ");
+
+	}
+
+}
