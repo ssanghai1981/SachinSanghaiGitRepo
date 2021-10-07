00d302690058e614996eafa2f11ffec080953c2b
Sachin Sanghai <Sanghai.sachin@gmail.com>
2021-10-07 23:11:33 +0530

    Added comments in second class file

--- GitTestProject/src/testPackage/TestClass2.java
+++ GitTestProject/src/testPackage/TestClass2.java
@@ -5,6 +5,7 @@ public class TestClass2 {
 	public static void main(String[] args) {
 	
 		System.out.println("Adding details into my first branch");
+		System.out.println("Added by Sachin Sanghai");
 
 	}
 
